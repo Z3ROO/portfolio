fea23d8d13115bb9c66aa1b7604fea110984e44a
zero <theloostt@live.com>
2023-06-28 18:38:08 -0300

    feat: projects main api route

---
+++ app/api/projects/route.ts
@@ -0,0 +1,19 @@
+import ProjectsRepo from "@/lib/repository/ProjectsRepo";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { name, description } = await req.json();
+  
+  await ProjectsRepo.insertOne({
+    name, description
+  });
+  
+  const body = JSON.stringify({
+    ok: true
+  });
+
+  return new NextResponse(body, {
+    status: 200
+  })
+}
+
