d7a92ea6d7d6b467b661e0737cb01b92a20cb49d
zero <theloostt@live.com>
2023-08-15 18:43:26 -0300

    feat: change title

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -5,7 +5,7 @@ export default function HomeLayout({children}: {children: ReactNode}) {
     <div>
       <div className='pt-8 pl-8 pb-4 w-min select-none'>
         <h2 className='text-5xl font-bold whitespace-nowrap'>Pedro Ribeiro</h2>
-        <h4 className='text-xl text-end text-red-300'>Software engineer</h4>
+        <h4 className='text-xl text-end text-red-300'>FullStack Developer</h4>
       </div>
       <div className='h-full w-full'>
         {children}
