87e68c6de28a880a28501f7d8dee2b97044b112a
zero <theloostt@live.com>
2023-08-15 18:42:28 -0300

    feat: apply select none in header

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function HomeLayout({children}: {children: ReactNode}) {
   return (
     <div>
-      <div className='pt-8 pl-8 pb-4 w-min'>
+      <div className='pt-8 pl-8 pb-4 w-min select-none'>
         <h2 className='text-5xl font-bold whitespace-nowrap'>Pedro Ribeiro</h2>
         <h4 className='text-xl text-end text-red-300'>Software engineer</h4>
       </div>
