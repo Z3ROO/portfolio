f4b19a70539d442d6628e00e6b07ad3eafbe47de
zero <theloostt@live.com>
2023-08-15 21:19:04 -0300

    style: organize better button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,5 +5,12 @@ interface CustomButton extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export function Button(props: CustomButton) {
-  return <button {...props} className={"px-4 py-2 border hover:border-opacity-80 transition-all border-opacity-30 border-red-500 bg-gradient-to-bl from-gray-700 to-gray-800 rounded-sm "+props.className}>{props.children}</button>
+  return <button 
+      {...props} 
+      className={`
+        px-4 py-2 rounded-sm border hover:border-opacity-80 
+        transition-all border-opacity-30 
+        border-red-500 bg-gradient-to-bl from-gray-700 to-gray-800 
+      `+props.className}
+      >{props.children}</button>
 }
