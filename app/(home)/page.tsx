105349a51cf42c4624f00876d6ab52443d90972f
zero <theloostt@live.com>
2023-08-15 21:40:19 -0300

    style: restructure home page componeent

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,35 +4,25 @@ import Link from "next/link";
 import GetAllProjectIds from "@/util/GetAllProjectIds";
 import GetProject from "@/util/GetProject";
 
-
 export default async function Home() {
-
   return (
-    <main className="">
+    <main className="flex flex-col items-center">
+      <SortingSection />
       <ProjectsShowcase />
     </main>
   )
 }
 
-
 async function ProjectsShowcase() {
   const ids = await GetAllProjectIds();
   
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex w-10/12 justify-end px-4 py-2">
-        <div className="flex justify-between w-48 self-end mr-8">
-          <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Complexity</span>
-          <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Difficulty</span>
-          <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Size</span>
-        </div>
-        <input className="w-72 h-8 px-2 py-1 rounded-sm shadow-inner shadow-[#0000002f] border border-opacity-30 hover:border-opacity-50 focus:border-opacity-75 outline-none border-red-500 bg-gray-800 focus:bg-gray-700 transition-all" />
-      </div>
-      <div className="flex w-10/12 flex-wrap">
-        {ids.map(id => {
+    <div className="flex w-10/12 flex-wrap">
+      {
+        ids.map(id => {
           return <ProjectDisplay key={id} id={id}/>
-        })}
-      </div>
+        })
+      }
     </div>
   );
 }
@@ -60,3 +50,15 @@ async function ProjectDisplay(props: {id: string}) {
   );
 }
 
+function SortingSection() {
+  return (
+    <div className="flex w-10/12 justify-end px-4 py-2">
+      <div className="flex justify-between w-48 self-end mr-8">
+        <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Complexity</span>
+        <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Difficulty</span>
+        <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Size</span>
+      </div>
+      <input className="w-72 h-8 px-2 py-1 rounded-sm shadow-inner shadow-[#0000002f] border border-opacity-30 hover:border-opacity-50 focus:border-opacity-75 outline-none border-red-500 bg-gray-800 focus:bg-gray-700 transition-all" />
+    </div>
+  )
+}
