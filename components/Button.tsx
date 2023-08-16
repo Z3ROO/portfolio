36f823d1c1acca68f33a35c49879fe063c0e412a
zero <theloostt@live.com>
2023-08-15 21:42:32 -0300

    style: small reorganization of tailwind classes

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,9 +8,9 @@ export function Button(props: CustomButton) {
   return <button 
       {...props} 
       className={`
-        px-4 py-2 rounded-sm border hover:border-opacity-80 
-        transition-all border-opacity-30 
-        border-red-500 bg-gradient-to-bl from-gray-700 to-gray-800 
+        px-4 py-2 rounded-sm transition-all 
+        border hover:border-opacity-80 border-opacity-30 border-red-500 
+        bg-gradient-to-bl from-gray-700 to-gray-800 
       `+props.className}
       >{props.children}</button>
 }
