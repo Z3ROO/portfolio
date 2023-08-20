977d885486d36a3a1927275da14f4ba5e561276d
zero <theloostt@live.com>
2023-08-20 14:46:25 -0300

    feat: include tooltip for icons

--- components/IconList.tsx
+++ components/IconList.tsx
@@ -10,7 +10,7 @@ export default function IconList({technologies}: { technologies: string[] }) {
         technologies.map(tech => {
           const Icon = IconReference[tech] ?? IconReference['no-icon'];
           
-          return <Icon className="w-12 h-12 fill-red-400 text-red-400 m-0 p-2" key={tech} />
+          return <Icon className="w-12 h-12 fill-red-400 text-red-400 m-0 p-2" key={tech} title={tech} />
         })
       }
     </>
