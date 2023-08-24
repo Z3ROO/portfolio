a32c5708cfa4845c12b14fcc3ce6d95d825a3b5c
zero <theloostt@live.com>
2023-08-24 20:54:10 -0300

    feat: implement loading for project page and wrap modal page's

--- app/@modal/(.)projects/[project_id]/loading.tsx
+++ app/@modal/(.)projects/[project_id]/loading.tsx
@@ -1,8 +1,3 @@
-export default function Loading() {
-  return (
-  <div className="w-full h-full flex flex-col justify-center items-center">
-    <div className="loader border-red-400"></div>
-    <div className="text-2xl p-4 text-red-200">Loading...</div>
-  </div>
-  )
-}
+import Loading from "@/app/projects/loading"
+
+export default Loading
--- app/projects/loading.tsx
+++ app/projects/loading.tsx
@@ -1,3 +1,8 @@
 export default function Loading() {
-  return <h2 className="text-4xl font-bold">Loading...</h2>
+  return  (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="loader border-red-400"></div>
+      <div className="text-2xl p-4 text-red-200">Loading...</div>
+    </div>
+  )
 }
