778299a3fee93921cd27e0c05e08b37680cc33b4
zero <theloostt@live.com>
2023-08-24 16:00:51 -0300

    feat: minimal custom page for inbox

---
+++ app/projects/(projects-custom-body)/inbox.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function InboxCustomPage() {
+  return (
+    <div>
+      <Image src={`/projects/inbox/thumbnail.jpg`} width={1080} height={675} alt="overview" />
+    </div>
+  )
+}
