ac442e2f88d78fdbfad95b03b33365db62945953
zero <theloostt@live.com>
2023-08-25 19:06:50 -0300

    feat: implement sorting by priority in array of projects

--- util/GetAllProjects.tsx
+++ util/GetAllProjects.tsx
@@ -1,9 +1,14 @@
 import GetProject from './GetProject';
 import GetProjectsIds from './GetProjectsIds';
+import { ProjectMetadata } from './ProjectMetadataType';
 
-export default async function GetAllProject() {  
+export default async function GetAllProject(): Promise<ProjectMetadata[]> {  
   const ids = await GetProjectsIds();
   const projects = await Promise.all(ids.map((project_id) => GetProject(project_id)));
   
+  projects.sort((a: ProjectMetadata, b: ProjectMetadata) => {
+    return a.priority - b.priority
+  })
+
   return projects;
 }
