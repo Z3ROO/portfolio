66507c3960935f21b6b20f472f39a861fc7cbdfa
zero <theloostt@live.com>
2023-09-14 22:20:39 -0300

    feat: remove sorting options

--- app/(home)/SortingSection.tsx
+++ app/(home)/SortingSection.tsx
@@ -13,11 +13,6 @@ export function SortingSection() {
 
   return (
     <div className="flex w-10/12 justify-end px-4 py-2">
-      <div className="flex justify-between w-48 self-end mr-8">
-        <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Complexity</span>
-        <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Difficulty</span>
-        <span className="text-sm text-red-200 hover:text-red-300 cursor-pointer">Size</span>
-      </div>
       <div className="relative group">
         <input 
           className="w-72 h-8 px-2 py-1 rounded-sm shadow-inner shadow-[#0000002f] border border-opacity-30 group-hover:border-opacity-50 focus:border-opacity-75 outline-none border-red-500 bg-gray-800 focus:bg-gray-700 transition-all" 
