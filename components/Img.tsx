ee4223935db64f0ca641ce9f789109a9cc82d08b
zero <theloostt@live.com>
2023-09-19 12:07:06 -0300

    feat: wrap next/image component

---
+++ components/Img.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+interface ImgProps {
+  src: string
+  width: number
+  height: number
+  alt: string
+  className?: string
+  wClassName?: string
+}
+
+export default function Img(props: ImgProps) {
+  return (
+    <div style={{minWidth: props.width+'px', minHeight: props.height+'px'}}>
+      <Image {...props} alt={props.alt} className={`relative z-10 ${props.className}`} />
+    </div>
+  )
+}
--- components/project/custom-body/TextAndImageBlock.tsx
+++ components/project/custom-body/TextAndImageBlock.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import Text from './Text';
+import Img from "@/components/Img";
 
 export default function TextImageBlock(props: {children: string|string[], img: string, alt?: string, reverse?: boolean}) {
   const { children, img, alt, reverse } = props;
   return (
     <div className={`flex py-4 ${reverse && 'flex-row-reverse'}`}>
-      <Image className="p-4" src={img} width={500} height={500} alt={alt || ''}/>
+      <Img  className='p-4' src={img} width={500} height={500} alt={alt || ''}/>
       <Text text={children} />
     </div>
   )
