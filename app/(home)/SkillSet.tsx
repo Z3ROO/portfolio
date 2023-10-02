f19f900b03145a8d6a5507e8c56cad3fd6d46a27
zero <theloostt@live.com>
2023-10-01 23:34:50 -0300

    feat: implement skillset cards with partial responsiveness

---
+++ app/(home)/SkillSet.tsx
@@ -0,0 +1,49 @@
+import { ReactNode } from 'react';
+import { IconType } from 'react-icons';
+import { BiLogoTypescript } from 'react-icons/bi';
+import { MdSecurity, MdScreenshotMonitor, MdWeb, MdArchitecture } from 'react-icons/md'
+import { PiPathBold } from 'react-icons/pi';
+import { FiArrowUpRight } from 'react-icons/fi';
+
+export default function SkillSet() {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 w-full p-8'>
+      <Skill Icon={BiLogoTypescript} title='Fluent Typescript'>
+        Any technology, framework or library derived from Javascript sinks in intuitevelly.
+      </Skill>
+      <Skill Icon={MdWeb} title='Web Development'>
+        Web development is easy they said. Have you tried to deliver responsive images?
+      </Skill>
+      <Skill Icon={MdSecurity} title='Web Security/Hacking'>
+        To keep one safe, one must know the existing threats and one must know how to threat.
+      </Skill>
+      <Skill Icon={MdScreenshotMonitor} title='All Screens'>
+        Not just responsive design, but all platforms, leveraging web based technologies.
+      </Skill>
+      <Skill Icon={PiPathBold} title='UX Design'>
+        An alchemy of aesthetics and usability, shaping digital journeys that gets the job done without trying.
+      </Skill>
+      <Skill Icon={MdArchitecture} title='Design and Architecture'>
+        A holistic view of the craft makes everything easier.
+      </Skill>
+    </div>
+  )
+}
+
+function Skill({Icon, children, title}: {Icon: IconType, children: ReactNode, title: string}) {
+  return (
+    <div className='flex flex-col p-4 rounded-sm bg-gray-600 shadow-md w-full sm:w-64 md:w-72 h-min md:h-72 mx-auto select-none cursor-pointer group transition-all'>
+      <div className='text-red-400 text-3xl md:text-6xl mb-2'>
+        <Icon />
+      </div>
+      <p className='text-xl text-red-400 my-2 border-t-2 border-red-400 w-min whitespace-nowrap'>{title}</p>
+      <p className='hidden md:block grow'>{children}</p>
+      <div className='text-red-300 text-right group-hover:text-red-400 transition-all'>
+        <span>Learn more</span>
+        <span className='text-red-200 group-hover:text-red-400 transition-all'>
+          <FiArrowUpRight className='inline-block' />
+        </span>
+      </div>
+    </div>
+  )
+}
