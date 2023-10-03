a6b8debec5a4680cbf8a0821e8f6e5867c6d18e9
zero <theloostt@live.com>
2023-10-03 13:36:59 -0300

    feat: remove red filter from thumbnails

--- app/(home)/ProjectDisplay.tsx
+++ app/(home)/ProjectDisplay.tsx
@@ -15,10 +15,10 @@ export function ProjectDisplay(props: {project: ProjectMetadata}) {
             <div className="flex flex-col">  
                 <h3 className="text-5xl font-bold break-words text-red-300 p-2 pb-6">{project.name}</h3>
                 <div className="relative">
-                  <div className="absolute bg-red-400 w-full h-full bg-opacity-30 group-hover:bg-opacity-0 transition-all duration-500"></div>
+                  
                   <Image 
                     src={`/projects/${project.id}/thumbnail.jpg`} 
-                    className="opacity-50 grayscale group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-500" 
+                    className="opacity-50 group-hover:opacity-100 transition-all duration-500" 
                     width={720}  height={450} alt="thumbnail" 
                   />
                 </div>
